7fb3b62883e8c6752e37c877093f3eb788fab0bf
jiang,Alan P <zhangxiao@zhangxiaodeMacBook-Pro.local>
2026-03-22 22:07:50 +0800

    update

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GOROOT=/Users/zhangxiao/softwares/go
 export GO111MODULE='on'
-export GOPATH=/Users/zhangxiao/workspace/go-zero-rpc-nacos
+export GOPATH=/Users/zhangxiao/workspace/
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 export GOPROXY=mirrors.aliyun.com/goproxy/
